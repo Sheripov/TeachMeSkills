b3765980c5150d402d34a401c1ad237c7b45cb7f
sheripov farruh <sheripov.farruh@gmail.com>
2020-08-03 00:23:34 +0300

    add hw-10

--- HomeWork/task_10/task_10_8.py
+++ HomeWork/task_10/task_10_8.py
@@ -5,5 +5,22 @@
 # С помощью функций создать файл с информацией о
 # товарах(Имя товара, цена, количество, комментарий).
 # Прочесть файл, Добавить новую позицию в конец. Удалить третью строку.
-from csv_utils import test
-test()
+import csv_utils
+
+csv_utils.csv_writer([
+    ['product name', 'price', 'quantity', 'comment'],
+    ['car', '342', '6', 'fsdgjrtyj'],
+    ['mr.bean', '2222', '1', 'sdasda'],
+    ['dfsf', '4545', '8786', 'jghdjdkhjg'],
+    ['mr.sadwer', '5647', '5', 'hfnfgjsdgf'],
+    ['fdsfmrnm', '7856', '65', '1242134erger'],
+    ['mr.bean', '2222', '1', 'sdasda'],
+], 'test_csv.csv')
+# csv_utils.csv_position_write('Liverpool', 'test_csv.csv', 3)
+#
+
+test = csv_utils.csv_read('test_csv.csv')
+print(test)
+csv_utils.csv_position_write(['car', '342', '6', 'fsdgjrtyj'], 'test_csv.csv')
+csv_utils.csv_position_delete('test_csv.csv', 2)
+
--- HomeWork/task_10/task_10_9.py
+++ HomeWork/task_10/task_10_9.py
@@ -4,3 +4,9 @@
 # 2) Создать функцию поиска самого дорогого товара.
 # 3) Создать функцию самого дешевого товара.
 # 4) Создать функцию уменьшения количества товара(на n, по-умолчанию на 1)
+import csv_utils
+
+print(csv_utils.csv_sum_price('test_csv.csv'))
+print(csv_utils.search_max_price('test_csv.csv'))
+print(csv_utils.search_min_price('test_csv.csv'))
+csv_utils.decrease_quantity('test_csv.csv')
