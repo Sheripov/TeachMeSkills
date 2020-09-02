40ba51925408f4588f2babe60a16b967ac54b6da
sheripov farruh <sheripov.farruh@gmail.com>
2020-09-02 12:53:16 +0300

    update

--- HomeWork/task_10/task_10_1.py
+++ HomeWork/task_10/task_10_1.py
@@ -15,12 +15,12 @@ except FileNotFoundError:
     my_file = open('test.txt', 'r', encoding='utf8')
 lines = my_file.readlines()
 my_file.close()
-# a) первая строка файла;
+"""a) первая строка файла"""
 print(lines[0])
-# b) пятая строка файла;
+"""b) пятая строка файла"""
 print(lines[3])
-# c) первые пять строк файла;
+"""c) первые пять строк файла"""
 print(''.join(lines[0:5]))
-# d) строки 1:2 файла;
+"""d) строки 1:2 файла"""
 print(''.join(lines[0:2]))
 print(''.join(lines))
