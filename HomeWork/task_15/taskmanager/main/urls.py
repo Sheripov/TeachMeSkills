815a7a516a00dddac696396330ddbeec9a874bc8
Farruh Sheripov <sheripov.farruh@gmail.com>
2020-09-03 17:28:27 +0300

    update

--- HomeWork/task_15/taskmanager/main/urls.py
+++ HomeWork/task_15/taskmanager/main/urls.py
@@ -4,4 +4,6 @@ from django.urls import path
 urlpatterns = [
     path('', views.index, name='index'),
     path('add_product', views.add_product, name='add_product'),
+    path('edit/<int:id>/', views.edit, name='edit'),
+    path('delete/<int:id>/', views.delete),
 ]
--- HomeWork/task_15/taskmanager/main/views.py
+++ HomeWork/task_15/taskmanager/main/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponseNotFound, HttpResponseRedirect
 from django.shortcuts import render, redirect
 from .models import Product
 from .forms import ProductForm
@@ -24,3 +25,30 @@ def add_product(request):
         'error': error
     }
     return render(request, 'main/add_product.html', context)
+
+
+# изменение данных в бд
+def edit(request, id):
+    try:
+        product = Product.objects.get(id=id)
+
+        if request.method == "POST":
+            product.name = request.POST.get("name")
+            product.price = request.POST.get("price")
+            product.quantity = request.POST.get("quantity")
+            product.comment = request.POST.get("comment")
+            product.save()
+            return HttpResponseRedirect("/")
+        else:
+            return render(request, "main/edit.html", {"product": product})
+    except Product.DoesNotExist:
+        return HttpResponseNotFound("<h2>Product not found</h2>")
+
+
+def delete(request, id):
+    try:
+        product = Product.objects.get(id=id)
+        product.delete()
+        return HttpResponseRedirect("/")
+    except Product.DoesNotExist:
+        return HttpResponseNotFound("<h2>Product not found</h2>")
