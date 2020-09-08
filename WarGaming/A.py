b852dbe8519b02a45b222dcffcf47b85b074a6da
Farruh Sheripov <sheripov.farruh@gmail.com>
2020-09-08 19:41:59 +0300

    update

--- WarGaming/A.py
+++ WarGaming/A.py
@@ -1,12 +1,24 @@
-inp = input().split(" ")
+inp = "6 12".split(" ")
 n = int(inp[0])
 k = int(inp[1])
-r = range(1, min(n, k) + 1)
-count = 0
 
-if 1 <= n <= 10 ** 5 and 1 <= k <= 10 ** 9:
-    matrix = (i * j for i in r for j in r)
-    for i in matrix:
-        if i == k:
-            count += 1
-print(count)
+
+def mult(n, k):
+    r = [i for i in range(1, min(n, k))]
+    print(r)
+    count = 0
+    if 1 <= n <= 100000 and 1 <= k <= 1000000000:
+        # matrix = (i * j for i in r for j in r)
+        # matrix.sort()
+        for i in r:
+            if i > k:
+                break
+            for j in r:
+                if j > k:
+                    break
+                if i * j == k:
+                    count += 1
+    return count
+
+
+mult(n, k)
