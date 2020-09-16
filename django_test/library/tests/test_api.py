f74cbb6257273ab636785d2696a75ee79c7fc6b8
Farruh Sheripov <sheripov.farruh@gmail.com>
2020-09-16 09:16:14 +0300

    REST_API: Farruh Sheripov

--- django_test/library/tests/test_api.py
+++ django_test/library/tests/test_api.py
@@ -5,8 +5,8 @@ from books.models import Books
 from books.serializers import BookSerializer
 
 
-class TestBookListAPIView(APITestCase):
-    def test_get(self):
+class BookListAPIViewTest(APITestCase):
+    def test_get_book_list(self):
         book_1 = Books.objects.create(
             title='Test1 Titile1',
             author_name='Test1 author1',
@@ -20,5 +20,19 @@ class TestBookListAPIView(APITestCase):
         url = reverse('books-list')
         response = self.client.get(url)
         serializer_data = BookSerializer([book_1, book_2], many=True).data
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
+        self.assertEqual(serializer_data, response.data)
+
+
+class BookDetailAPIViewTest(APITestCase):
+    def test_get_book_details(self):
+        book_1 = Books.objects.create(
+            title='Test1 Titile1',
+            author_name='Test1 author1',
+            description='Test1 description1'
+        )
+        url = reverse('books-detail')
+        response = self.client.get(url)
+        serializer_data = BookSerializer(book_1).data
         self.assertEqual(status.HTTP_200_OK, response.status_code)
         self.assertEqual(serializer_data, response.data)
